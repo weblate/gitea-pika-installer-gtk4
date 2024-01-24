446ebcb4e093fe424dac711a90a0a242c1fa8b8d
Ward from fusion-voyager-3 <hotrod.master@hotmail.com>
2024-01-24 21:14:59 +0300

    fix

--- data/scripts/automatic-partition-install.sh
+++ data/scripts/automatic-partition-install.sh
@@ -4,7 +4,7 @@ set -e
 
 DISK="$(cat "/tmp/pika-installer-gtk4-target-auto.txt")"
 LOCALE="$(cat "/tmp/pika-installer-gtk4-lang.txt")"
-KEYBOARD="$(cat "/tmp/pika-installer-gtk4-target-auto.txt")"
+KEYBOARD="$(cat "/tmp/pika-installer-gtk4-keyboard.txt")"
 TIMEZONE="$(cat "/tmp/pika-installer-gtk4-timezone.txt")"
 
 touch "/tmp/pika-installer-gtk4-status-parting.txt"
