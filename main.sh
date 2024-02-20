5595066af3f6c545f1c59f42cb67dff77352d04b
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-02-20 23:20:10 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,6 +5,7 @@ cd ./pika-installer-gtk4
 
 # Get build deps
 apt-get build-dep ./ -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # Build package
 dpkg-buildpackage --no-sign
